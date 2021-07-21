982ad780351d6e3d9fd71ef016e1172686de69fc
NolenJ3 <60423538+NolenJ3@users.noreply.github.com>
2021-07-20 17:24:34 -0700

    Add files via upload

--- src/com/company/SchedulingButtons.java
+++ src/com/company/SchedulingButtons.java
@@ -1,4 +1,48 @@
-package com.company;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.Scanner;
 
 public class SchedulingButtons {
+    private JPanel panel;
+    private CalendarModel m;
+
+    SchedulingButtons(CalendarModel model){
+        m = model;
+        panel = new JPanel();
+        JButton createButton = new JButton("Create");
+        JButton fromFileButton = new JButton("fromFile");
+        panel.add(createButton);
+        panel.add(fromFileButton);
+
+
+        /**
+         * action listener for createButton pressed
+         * edit later on for time conflict
+         */
+        createButton.addActionListener(e -> {
+            String name = JOptionPane.showInputDialog("Name: ");
+            String date = JOptionPane.showInputDialog("Date: MM/DD/YYYY");
+            String startT = JOptionPane.showInputDialog("Starting Time: HH") + ":00";
+            String endT = JOptionPane.showInputDialog("Ending Time: HH") + ":00";
+            Event E = new Event(name,date,startT,endT);
+            m.addEvent(E);
+
+                }
+                );
+
+        fromFileButton.addActionListener(e-> {
+            String filename = JOptionPane.showInputDialog("File Name: ");
+            m.loadFile(filename);
+
+                }
+                );
+
+    }
+
+    public JPanel getPanel(){
+        return panel;
+    }
 }
--- src/com/company/SchedulingButtonsTester.java
+++ src/com/company/SchedulingButtonsTester.java
@@ -1,4 +1,15 @@
-package com.company;
+import javax.swing.*;
+import java.awt.*;
 
 public class SchedulingButtonsTester {
+    public static void main(String[] args) {
+
+
+        JFrame frame = new JFrame();
+        SchedulingButtons buttons = new SchedulingButtons();
+        frame.add(buttons.getPanel());
+        frame.setSize(300,300);
+        frame.setLayout(new FlowLayout());
+        frame.setVisible(true);
+    }
 }
