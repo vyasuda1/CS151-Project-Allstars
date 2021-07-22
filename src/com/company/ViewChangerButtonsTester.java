073483ef4daa3ef54c7c2723b1147324692392e5
Haider-01 <80722085+Haider-01@users.noreply.github.com>
2021-07-22 12:40:51 -0700

    Updated ViewChangerButtons Tester
     to be completely implemented

--- src/com/company/ViewChangerButtonsTester.java
+++ src/com/company/ViewChangerButtonsTester.java
@@ -1,15 +1,30 @@
 package com.company;
 /**
- *
+ * -Description of file-
  * @author Haider Almandeel
  * @version 1.0 7/21/2021
  */
 
+import javax.swing.*;
+
 /**
- *
+ * To compile, comment out call to notifyCalendarView() in setEventsToView() in CalendarModel.java
  */
 public class ViewChangerButtonsTester {
+    /**
+     * -Desciprtion of method-
+     * @param args -Description of args-
+     */
     public static void main(String[] args) {
-
+        CalendarModel model = new CalendarModel();
+        JFrame frame = new JFrame();
+        ViewChangerButtons viewChangerButtons = new ViewChangerButtons(model);
+        JPanel viewButtons = viewChangerButtons.getPanel();
+        frame.add(viewButtons);
+        frame.setTitle("ViewChangerButtonsTester.java");
+        frame.setSize(300, 100);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
