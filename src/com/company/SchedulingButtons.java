9bb876148972154b765fa2cd4235bed4562db160
NolenJ3 <60423538+NolenJ3@users.noreply.github.com>
2021-07-22 14:47:53 -0700

    Update SchedulingButtons.java
    Updated with conflict code, will add javadocs

--- src/com/company/SchedulingButtons.java
+++ src/com/company/SchedulingButtons.java
@@ -5,52 +5,57 @@ package com.company;
  * @version 1.0 7/20/21
  */
 import javax.swing.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
-/**
- *
- */
 public class SchedulingButtons {
     private JPanel panel;
-    private CalendarModel m; //consider changing from m to model
+    private CalendarModel m;
 
-    /**
-     *
-     * @param model
-     */
-    public SchedulingButtons(CalendarModel model) {
+    SchedulingButtons(CalendarModel model){
         m = model;
         panel = new JPanel();
+        AtomicInteger option = new AtomicInteger();
+        AtomicBoolean doesConflict = new AtomicBoolean(false);
         JButton createButton = new JButton("Create");
         JButton fromFileButton = new JButton("fromFile");
         panel.add(createButton);
         panel.add(fromFileButton);
 
-        /**
-         * action listener for createButton pressed
-         * edit later on for time conflict
-         */
-        createButton.addActionListener(e -> {
-            String name = JOptionPane.showInputDialog("Name: ");
-            String date = JOptionPane.showInputDialog("Date: MM/DD/YYYY");
-            String startT = JOptionPane.showInputDialog("Starting Time: HH") + ":00";
-            String endT = JOptionPane.showInputDialog("Ending Time: HH") + ":00";
-            Event E = new Event(name, date, startT, endT);
-            m.addEvent(E);
-        });
 
         /**
-         *
+         * action listener for createButton pressed
          */
+        createButton.addActionListener(e -> {
+            do {
+                String name = JOptionPane.showInputDialog("Name: ");
+                String date = JOptionPane.showInputDialog("Date: MM/DD/YYYY");
+                String startT = JOptionPane.showInputDialog("Starting Time: HH") + ":00";
+                String endT = JOptionPane.showInputDialog("Ending Time: HH") + ":00";
+                Event newEvent = new Event(name, date, startT, endT);
+                for (Event event : m.getEvents()){
+                    if (event.conflicts(newEvent)){
+                        doesConflict.set(true);
+                        option.set(JOptionPane.showConfirmDialog(null, "Would you like try to create another event", "New Event ?", JOptionPane.YES_NO_OPTION));
+
+                    }
+                }
+            } while(option.get() == 1);
+            if (!doesConflict.get()) {
+                m.addEvent(newEvent);
+            }
+
+        });
+
         fromFileButton.addActionListener(e-> {
             String filename = JOptionPane.showInputDialog("File Name: ");
             m.loadFile(filename);
-        });
+
+                }
+                );
+
     }
 
-    /**
-     *
-     * @return
-     */
     public JPanel getPanel(){
         return panel;
     }
