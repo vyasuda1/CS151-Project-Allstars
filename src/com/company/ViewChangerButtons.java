d7a7a2d12dd929d190ea84f53522e849dd101b20
Viola Yasuda <viola.yasuda@sjsu.edu>
2021-07-25 22:49:51 -0700

    Updated ViewChangerButtons so the current view's button would be highlighted.

--- src/com/company/ViewChangerButtons.java
+++ src/com/company/ViewChangerButtons.java
@@ -5,6 +5,7 @@ package com.company;
  * @version 1.0 7/20/2021
  */
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Represents the view-changing buttons in our calendar program.
@@ -25,13 +26,35 @@ public class ViewChangerButtons {
         JButton monthButton = new JButton("Month");
         JButton agendaButton = new JButton("Agenda");
 
-        dayButton.addActionListener(e -> model.setViewType("Day"));
-        weekButton.addActionListener(e -> model.setViewType("Week"));
-        monthButton.addActionListener(e -> model.setViewType("Month"));
+        dayButton.addActionListener(e -> {
+            model.setViewType("Day");
+            dayButton.setBackground(Color.YELLOW);
+            weekButton.setBackground(null);
+            monthButton.setBackground(null);
+            agendaButton.setBackground(null);
+        });
+        weekButton.addActionListener(e -> {
+            model.setViewType("Week");
+            dayButton.setBackground(null);
+            weekButton.setBackground(Color.YELLOW);
+            monthButton.setBackground(null);
+            agendaButton.setBackground(null);
+        });
+        monthButton.addActionListener(e -> {
+            model.setViewType("Month");
+            dayButton.setBackground(null);
+            weekButton.setBackground(null);
+            monthButton.setBackground(Color.YELLOW);
+            agendaButton.setBackground(null);
+        });
         agendaButton.addActionListener(e -> {
             String startDate = JOptionPane.showInputDialog("Enter Start Date (MM/DD/YYYY) : ");
             String endDate = JOptionPane.showInputDialog("Enter End Date (MM/DD/YYYY): ");
             model.setViewType(startDate, endDate);
+            dayButton.setBackground(null);
+            weekButton.setBackground(null);
+            monthButton.setBackground(null);
+            agendaButton.setBackground(Color.YELLOW);
         });
 
         panel.add(dayButton);
