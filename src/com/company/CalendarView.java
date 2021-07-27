1d1cedea2b6c2080585c953ffcb6bed4f9945e26
Viola Yasuda <viola.yasuda@sjsu.edu>
2021-07-26 20:20:41 -0700

    Updated the GUI layout to GridBagLayout in CalendarView

--- src/com/company/CalendarView.java
+++ src/com/company/CalendarView.java
@@ -14,6 +14,11 @@ import java.awt.*;
 public class CalendarView {
     private final CalendarModel model;
     private final JTextArea textArea;
+    public static final double ROW_1_WEIGHT = 0.1;
+    public static final double ROW_2_WEIGHT = 0.1;
+    public static final double ROW_3_WEIGHT = 0.8;
+    public static final double ROW_4_WEIGHT = 0.1;
+    public static final double COLUMN_WEIGHT = 0.5;
 
     /**
      * Constructs a CalendarView object.
@@ -43,19 +48,64 @@ public class CalendarView {
         CurrentCalendarComponent currentCalendarComponent = new CurrentCalendarComponent(model);
         JPanel currentCalendar = currentCalendarComponent.getPanel();
 
+        JButton dateFirstFormatterButton = new JButton("Date First");
+        JButton timeFirstFormetterButton = new JButton("Time FIrst");
+        JPanel formatterPanel = new JPanel();
+        formatterPanel.add(dateFirstFormatterButton);
+        formatterPanel.add(timeFirstFormetterButton);
+
         //code for the frame
         JFrame frame = new JFrame();
-        //frame.setLayout(new FlowLayout());
         frame.setTitle("Allstars Calendar Program");
-        frame.setSize(800, 500);
+        frame.setSize(800, 600);
         frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        frame.add(dateButtons, BorderLayout.NORTH);
-        frame.add(viewButtons, BorderLayout.SOUTH);
-        frame.add(scheduleButtons, BorderLayout.EAST);
-        frame.add(scrollPane, BorderLayout.CENTER);
-        frame.add(currentCalendar, BorderLayout.WEST);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.weightx = COLUMN_WEIGHT;
+        gridBagConstraints.weighty = ROW_1_WEIGHT;
+        frame.add(dateButtons, gridBagConstraints);
+
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.weightx = COLUMN_WEIGHT;
+        gridBagConstraints.weighty = ROW_1_WEIGHT;
+        frame.add(viewButtons, gridBagConstraints);
+
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.weightx = COLUMN_WEIGHT;
+        gridBagConstraints.weighty = ROW_2_WEIGHT;
+        gridBagConstraints.fill = GridBagConstraints.VERTICAL;
+        frame.add(scheduleButtons, gridBagConstraints);
+
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridheight = 2;
+        gridBagConstraints.weightx = COLUMN_WEIGHT;
+        gridBagConstraints.weighty = 0.1;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        gridBagConstraints.insets = new Insets(0, 25, 0, 25);
+        frame.add(scrollPane, gridBagConstraints);
+
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.gridheight = 1;
+        gridBagConstraints.weightx = COLUMN_WEIGHT;
+        gridBagConstraints.weighty = ROW_3_WEIGHT;
+        gridBagConstraints.insets = new Insets(0, 25, 25, 25);
+        frame.add(currentCalendar, gridBagConstraints);
+
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.weightx = COLUMN_WEIGHT;
+        gridBagConstraints.weighty = ROW_4_WEIGHT;
+        frame.add(formatterPanel, gridBagConstraints);
     }
 
     /**
