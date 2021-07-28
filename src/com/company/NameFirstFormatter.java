506ca886ed5bf42a7cf365b60d2f650954aa3de1
Haider-01 <80722085+Haider-01@users.noreply.github.com>
2021-07-27 17:10:50 -0700

    updated namefirst formatter

--- src/com/company/NameFirstFormatter.java
+++ src/com/company/NameFirstFormatter.java
@@ -1,19 +1,16 @@
 package com.company;
 
 /**
- * File javadoc (description here)
- * @author
- * @version
+ * This file contains a class that implements EventFormatter that formats the event in a name first format.
+ * @author Haider Almandeel
+ * @version 1.0 7/27/2021
  */
 
-/**
- * Class javadoc (description here)
- */
 public class NameFirstFormatter implements EventFormatter{
     /**
-     * description here
-     * @param event
-     * @return
+     * Returns a string swapping the time and event with the name being shown first.
+     * @author Haider Almandeel
+     * @version 1.0 7/27/2021
      */
     @Override
     public String formatEvent(Event event) {
