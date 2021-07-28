624e4c9d1dbd85437847c27c2373536f387004d3
Haider-01 <80722085+Haider-01@users.noreply.github.com>
2021-07-27 17:10:17 -0700

    updated working timefirstformatter

--- src/com/company/TimeFirstFormatter.java
+++ src/com/company/TimeFirstFormatter.java
@@ -1,21 +1,19 @@
 package com.company;
-/**
- * File javadoc (description here)
- * @author
- * @version
- */
 
 /**
- * Class javadoc (description here)
+ * This file contains a class that implements EventFormatter that formats the event in a time first format.
+ * @author Haider Almandeel, Viola Yasuda
+ * @version 1.0 7/27/2021
  */
+
 public class TimeFirstFormatter implements EventFormatter{
     /**
-     * description here
-     * @param event
-     * @return
+     * Returns a string swapping the time and event with the time being shown first.
+     * @author Haider Almandeel, Viola Yasuda
+     * @version 1.0 7/27/2021
      */
     @Override
     public String formatEvent(Event event) {
-        return null;
+        return event.getTimeInterval().toString() + ": " + event.getName() + "\n";
     }
 }
